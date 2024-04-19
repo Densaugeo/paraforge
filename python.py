d748029290735c41b8a2efbf9d1d295fb9af75a6
Densaugeo <>
2024-04-18 20:28:18 -0400

    Getting started on a data access model

--- python.py
+++ python.py
@@ -3,9 +3,20 @@ import cffi
 ffi = cffi.FFI()
 
 ffi.cdef('''
-    uint32_t add(uint32_t left, uint32_t right);
+    uint32_t new_data_structure();
+    uint32_t multiply_float(uint32_t index, float value);
+    float get_float(uint32_t index);
 ''')
 
 C = ffi.dlopen('target/debug/libparaforge.so')
 
-print(C.add(1, 2))
+index = C.new_data_structure()
+print('Initial float value:', C.get_float(index))
+C.multiply_float(index, 2.0)
+print('Tried doubling it:', C.get_float(index))
+C.multiply_float(index, 0.5)
+print('Tried putting it back:', C.get_float(index))
+
+print('What happens when I access a structure that doesn\'t exist?',
+    C.get_float(4))
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,45 @@
-#[no_mangle]
-pub extern "C" fn add(left: u32, right: u32) -> u32 {
-    left + right
+use std::sync::Mutex;
+
+struct DataStructure {
+  an_integer: i32,
+  a_float: f32,
+  a_string: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl DataStructure {
+  pub fn new() -> Self {
+    Self {
+      an_integer: 4,
+      a_float: 1.2,
+      a_string: String::from("Stringyyyyyy!"),
     }
+  }
+}
+
+static DATA_STRUCTURES: Mutex<Vec<DataStructure>> = Mutex::new(Vec::new());
+
+#[no_mangle]
+pub extern "C" fn new_data_structure() -> u32 {
+  let mut unlocked = DATA_STRUCTURES.lock().unwrap();
+  unlocked.push(DataStructure::new());
+  (unlocked.len() - 1) as u32
+}
+
+#[no_mangle]
+pub extern "C" fn multiply_float(index: u32, value: f32) -> u32 {
+  let mut unlocked = DATA_STRUCTURES.lock().unwrap();
+  
+  // TODO I think error codes will have to be saved in an atomic for later
+  // retrieval
+  if unlocked.len() <= index as usize { return 1 };
+  
+  unlocked[index as usize].a_float *= value;
+  return 0;
+}
+
+#[no_mangle]
+pub extern "C" fn get_float(index: u32) -> f32 {
+  let unlocked = DATA_STRUCTURES.lock().unwrap();
+  
+  unlocked[index as usize].a_float
 }
