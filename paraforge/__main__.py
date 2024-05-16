7b2976a497436177a08a7fa3da47c2593ecd4324
Densaugeo <>
2024-05-16 19:25:35 -0400

    Temporary Python CLI runtime

--- paraforge/__main__.py
+++ paraforge/__main__.py
@@ -1,4 +1,4 @@
-import argparse, importlib
+import argparse, importlib, struct, json, sys
 
 import paraforge
 
@@ -21,5 +21,23 @@ script = importlib.util.module_from_spec(spec)
 sys.modules['script'] = script
 spec.loader.exec_module(script)
 
+paraforge.init()
 getattr(script, 'gen_' + args.generator)(*args.parameters)
-print('Serialization:', paraforge.serialize())
+glb = paraforge.serialize()
+glb_length = struct.unpack('<L', glb[8:12])[0]
+assert glb_length == len(glb)
+json_length = struct.unpack('<L', glb[12:16])[0]
+json_data = json.loads(glb[20:20 + json_length])
+if glb_length > json_length + 20:
+    bin_length = struct.unpack('<L', glb[20 + json_length:24 + json_length])[0]
+    bin_data = glb[28 + json_length:]
+    assert bin_length == len(bin_data)
+else:
+    bin_data = None
+
+if args.format.lower() == 'pretty':
+    print(json.dumps(json_data, indent=2, sort_keys=True))
+elif args.format.lower() == 'gltf':
+    raise NotImplementedError()
+elif args.format.lower() == 'glb':
+    sys.stdout.buffer.write(glb)
